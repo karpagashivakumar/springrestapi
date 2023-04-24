72be146d1c0d0579cc8f6040408051181facc59a
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2023-04-24 10:39:24 +0530

    Day2
    Value code

---
+++ Day2/src/main/java/com/example/Day1/Ipl.java
@@ -0,0 +1,22 @@
+package com.example.Day1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Ipl {
+	@Value("RCB")
+	private String teamName;
+	@Value("1")
+	private int tablePosition;
+	@Value("Eesala cup namade")
+	private String Quote;
+	@RequestMapping("/ji")
+	@ResponseBody
+	public String Data() {
+		return "Team Name : " +teamName+ " Position : " +tablePosition+ " Quote : "+Quote ;
+	}
+
+}
