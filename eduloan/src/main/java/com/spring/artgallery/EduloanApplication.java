746b6e9a56951003318f453cdf638cd1f5c0861a
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2023-05-29 10:48:36 +0530

    Day5

---
+++ eduloan/src/main/java/com/spring/artgallery/EduloanApplication.java
@@ -0,0 +1,13 @@
+package com.spring.artgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduloanApplication.class, args);
+	}
+
+}
